eb54690ae9bb3baf86ee0337a41870c1caec3363
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-12-04 22:34:27 +0300

    add calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,23 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b==0){
+            throw new IllegalArgumentException("Divide by zero error");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int a, int b, int c){ //53x - 6 = 0
+        int d = times(a,b);
+        int e = div(c,a);
+        int f = add(d,e);
+        return dif(f,a);
     }
 }
